f97e28371e468615c3bf90c450ca09353b855172
Yaroslava <gordislava@gmail.com>
2020-01-24 18:06:28 +0300

    somy styles for popup

--- source/js/script.js
+++ source/js/script.js
@@ -27,3 +27,37 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+
+
+
+var orderButton = document.querySelector(".promo__button");
+var productItem = document.querySelectorAll(".product-item__button");
+var modalPurchase = document.querySelector(".modal__container");
+var modalOverlay = document.querySelector(".modal");
+
+if (orderButton) {
+orderButton.addEventListener("click", function(evt) {
+evt.preventDefault();
+modalOverlay.classList.remove("visually-hidden");
+});
+}
+
+productItem.forEach(e =>
+e.addEventListener("click", function(evt) {
+evt.preventDefault();
+modalOverlay.classList.remove("visually-hidden");
+})
+);
+
+modalOverlay.addEventListener(
+"click",
+function(evt) {
+if (evt.currentTarget !== evt.target) {
+return;
+}
+
+modalOverlay.classList.add("visually-hidden");
+},
+false
+);
