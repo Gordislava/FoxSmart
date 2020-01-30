863601537f0bbb777a84ebae631b444a5745edec
Gordislava <gordislava@gmail.com>
2020-01-30 08:01:33 +0300

    some fixed

--- source/js/script.js
+++ source/js/script.js
@@ -64,3 +64,38 @@ modalOverlay.classList.add("visually-hidden");
 false
 );
 
+
+
+
+var formOrderButton = document.querySelectorAll(".js-form-button");
+var modalPurchase = document.querySelector(".modal__container");
+var modalOverlay = document.querySelector(".modal-info");
+var close = document.querySelector(".modal__close-button")
+
+
+
+formOrderButton.forEach(e =>
+    e.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modalOverlay.classList.remove("visually-hidden");
+    })
+);
+
+close.addEventListener(
+"click",
+
+    function(evt) {
+        if (evt.currentTarget !== evt.target) {
+        return;
+    }
+    modalOverlay.classList.add("visually-hidden");
+    }, false
+);
+
+var menuButton = document.querySelector(".header__menu-icon");
+var menu = document.querySelector(".header__main-menu");
+
+menuButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    menu.classList.toggle("menu__open");
+});
