050e8915f0e3d89784a54e6a4db686ec73159d96
Gordislava <gordislava@gmail.com>
2020-01-31 17:36:23 +0300

    mask, lazyload, final build

--- gulpfile.js
+++ gulpfile.js
@@ -7,100 +7,67 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var webp = require("gulp-webp");
 var server = require("browser-sync").create();
-var minify = require("gulp-csso");
+var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var del = require("del");
 var run = require("run-sequence");
+var jsmin = require("gulp-minify");
 
-// gulp.task("css", function () {
-//   return gulp.src("source/scss/first-screen.scss")
-//     .pipe(plumber())
-//     .pipe(sass())
-//     .pipe(postcss([
-//       autoprefixer()
-//     ]))
-//     .pipe(gulp.dest("build/css"))
-//     .pipe(server.stream());
-// });
+gulp.task("css-first", function () {
+  return gulp.src("source/scss/first-screen.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("first-screen.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
 
-// gulp.task("images", function () {
-//   return gulp.src("source/img/**/*.{png,jpg,svg}")
-//     .pipe(imagemin([
-//       imagemin.optipng({ optimizationLevel: 3 }),
-//       imagemin.jpegtran({ progressive: true }),
-//       imagemin.svgo()
-//     ]))
-//     .pipe(gulp.dest("source/img"));
-// });
+gulp.task("css-second", function () {
+  return gulp.src("source/scss/style.scss")
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(postcss([
+          autoprefixer()
+      ]))
+      .pipe(csso())
+      .pipe(rename("style.min.css"))
+      .pipe(gulp.dest("build/css"))
+});
 
-// gulp.task("webp", function () {
-//   return gulp.src("source/img/**/*.{png,jpg}")
-//     .pipe(webp({ quality: 90 }))
-//     .pipe(gulp.dest("source/img"));
-// });
+gulp.task("js", function () {
+  return gulp.src("source/js/script.js")
+    .pipe(plumber())
+    .pipe(jsmin())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(server.stream());
+});
 
-// // gulp.task("server", function () {
-// //   server.init({
-// //     server: "source/",
-// //     notify: false,
-// //     open: true,
-// //     cors: true,
-// //     ui: false
-// //   });
-// // });
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.html"
+  ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
 
-//   gulp.task("sprite", function () {
-//     return gulp.src("source/img/**/icon-*.svg")
-//       .pipe(svgstore({
-//         inlineSvg: true
-//       }))
-//       .pipe(rename("sprite.svg"))
-//       .pipe(gulp.dest("build/img"));
-//   });
-
-// gulp.task("copy", function () {
-//   return gulp.src([
-//     "source/fonts/**/*.{woff,woff2}",
-//     "source/img/**",
-//     "source/js/**",
-//     "source/*.html"
-//   ], {
-//       base: "source"
-//     })
-//     .pipe(gulp.dest("build"));
-// });
-
-// gulp.task("clean", function () {
-//   return del("build");
-// });
-
-// gulp.task("serve", function () {
-//   server.init({
-//     server: "build/",
-//     notify: false,
-//     open: true,
-//     cors: true,
-//     ui: false
-//   });
-
-//   gulp.watch("source/sass/**/*.{scss,sass}", gulp.parallel("css"));
-//   gulp.watch("source/*.html", gulp.parallel("copy"))
-//     .on("change", server.reload);
-// });
-
-// gulp.task("build", gulp.series("clean", "copy", "css", "sprite", function (done) {
-//   done ();
-// }));
-
-// gulp.task("start", gulp.series("css", "server"));
+gulp.task("clean", function () {
+  return del("build");
+});
 
 
-// gulp.watch("source/scss/**/*.{scss,sass}", gulp.series("css"));
-// gulp.watch("source/*.html").on("change", server.reload);
-// });
+
 
 gulp.task("css", function () {
   return gulp.src("source/scss/*.scss")
@@ -143,3 +110,8 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+
+gulp.task("build", gulp.series("clean", "copy", "css-first", "css-second", "js", function (done) {
+  done();
+}));
